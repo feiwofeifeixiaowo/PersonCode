31362609df47aa591440f9b9ed31f11457abe099
F <feiwofeifeixiaowo@gmail.com>
2016-12-18 15:44:49 +0800

    update

--- newFakeData/src/FakeData.scala
+++ newFakeData/src/FakeData.scala
@@ -3,8 +3,7 @@
     * 1.0 版本正常切分，并按ID合并GPS轨迹并存储到桌面
     */
 
-  import scala.collection.mutable.HashMap
-  import scala.collection.mutable.ArrayBuffer
+  import scala.collection.mutable._
   import org.apache.hadoop.fs.Path
   import org.apache.spark.{SparkConf, SparkContext}
   import org.apache.spark.{SparkConf, SparkContext}
@@ -18,31 +17,32 @@
       // Spark conf
       val conf      = new SparkConf().setAppName("FakeData")
       val sc        = new SparkContext(conf)
-      // lib of id
-      var idArray   = new ArrayBuffer[String]()
-      // make data structure like (id,[v1, v2, v3, ...])  FcdBusProxyMessage.2016-12-11.log
+
       println("begin parse old data ~")
-      val sourceRDD = sc.textFile("file:///Users/X/Desktop/temp.log").flatMap( line => {
+      // make data structure like (id,[v1, v2, v3, ...])  FcdBusProxyMessage.2016-12-11.log
+      val sourceRDD = sc.textFile("file:///Users/X/Git_Repo/PersonCode/data/FcdBusProxyMessage.2016-12-11.log")
+      val dataRDD = sourceRDD.flatMap( line => {
         val items             = line.split("\\*")
-        var retHashMap        = HashMap[String, String]()
+        var retArray   = new ArrayBuffer[Tuple2[String,String]]()
         val dateAndServerInfo = items(0)
         for (i <- 1 until  (items.length -1)) {
           val id      = items(i).split(",")(1)
           val value   = dateAndServerInfo + "*" + items(i)
-          idArray     +=id
-          retHashMap  +=((id, value))
+          retArray.append(Tuple2(id, value))
         }
-        retHashMap
-      }).reduceByKey(_+ "@" +_).sortByKey()
+        retArray
+      }).reduceByKey(_+ "@" +_)//.sortByKey()
+
       // out put to file
-      sourceRDD.saveAsTextFile("file:///Users/X/Desktop/midResult")
+//       dataRDD.saveAsTextFile("file:///Users/X/Desktop/midResult")
       // select new id in random way
       val r = scala.util.Random
-      val newID = idArray(r.nextInt(idArray.length - 1))
+      val idVec = dataRDD.collect().toVector
+      val newID = idVec(r.nextInt(idVec.length - 1))._1
       // make new data
       println("begin make new data ~")
-      val resultRDD = sourceRDD.flatMap(x=>{
-        var retHashMap        = HashMap[String, String]()
+      val resultRDD = dataRDD.flatMap(x=>{
+        var retArray   = new ArrayBuffer[Tuple2[String,String]]()
         val items = x._2.split("\\@")
         for (i <- 1 until  (items.length - 1)) {
           // tmpResult(0): 2016-12-11 00:00:00.474 - /125.88.128.197:60514:
@@ -52,15 +52,19 @@
           var gpsData       = tmpResult(1).split(",")
           // gpsData(0): 111
           // gpsDate(1): oldID
-          var newGPSData = "*" + gpsData(0) + newID
+          var newGPSData = "*" + gpsData(0) + "," +newID
           for (i <- 2 until(gpsData.length - 1)) {  // make new GPS location data
-            newGPSData += gpsData(i)
+            val tmp = "," + gpsData(i)
+            newGPSData += tmp
           }
           val newItem = newDateAndServer + newGPSData
-          retHashMap += ((newDateAndServer, newGPSData))
+          retArray.append(Tuple2(newDateAndServer, newGPSData))
         }
-        retHashMap
-      }).reduceByKey(_+_).sortByKey().saveAsTextFile("file:///Users/X/Desktop/newData")
+        retArray
+      }).reduceByKey(_+ "#" +_).sortByKey().map(line =>{
+        val retStr = line._1 + line._2 + "#"
+        retStr
+      }).repartition(1).saveAsTextFile("file:///Users/X/Desktop/newData")
       // print out Elapsed time
       println("Elapsed time: " + (System.nanoTime() - t0) / 1e9 + " s")
     }
